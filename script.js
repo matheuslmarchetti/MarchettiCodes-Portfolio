df0deb53120942d2edf44e61351d09149ce413f5
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-16 19:44:32 -0400

    Updates HTML, CSS e JS

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function changeTextH1ptbr() {
+function changeTextptbr() {
     document.querySelector("#textH1EnUS").innerHTML = "Escolha seu idioma.";
-    document.querySelector("#buttonportuguesetext").innerHTML = "Portugês (Brasil)";
+    document.querySelector("#buttonportuguesetext").innerHTML = "Português (Brasil)";
     document.querySelector("#buttonenglishtext").innerHTML = "Inglês (EUA)";
+    document.querySelector("#footertext").innerHTML = "Desenvolvido por Matheus L. Marchetti";
 }
-function changeTextH1enus() {
+function changeTextenus() {
     document.querySelector("#textH1EnUS").innerHTML = "Choose your language.";
     document.querySelector("#buttonportuguesetext").innerHTML = "Portuguese (Brazil)";
     document.querySelector("#buttonenglishtext").innerHTML = "English (USA)";
+    document.querySelector("#footertext").innerHTML = "Developed by Matheus L. Marchetti";
 }
