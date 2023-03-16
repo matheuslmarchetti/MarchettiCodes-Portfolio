3cf9423da738a171b6718f423ab193581442d4db
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-16 19:04:50 -0400

    Update JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeTextH1ptbr() {
+    document.querySelector("#textH1EnUS").innerHTML = "Escolha seu idioma.";
+    document.querySelector("#buttonportuguesetext").innerHTML = "Portugês (Brasil)";
+    document.querySelector("#buttonenglishtext").innerHTML = "Inglês (EUA)";
+}
+function changeTextH1enus() {
+    document.querySelector("#textH1EnUS").innerHTML = "Choose your language.";
+    document.querySelector("#buttonportuguesetext").innerHTML = "Portuguese (Brazil)";
+    document.querySelector("#buttonenglishtext").innerHTML = "English (USA)";
+}
