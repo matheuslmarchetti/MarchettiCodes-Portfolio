63f48c5d0f05aa686cee2deb97e8486cb43f2a11
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-17 14:28:43 -0400

    Updates

---
+++ portuguese/portuguese.js
@@ -0,0 +1,14 @@
+//Click Flag Redict Page Portuguese
+const OPPTBR = document.querySelector("select");
+OPPTBR.addEventListener("change", portuguesePage);
+    function portuguesePage() {
+        location.href = "../portuguese/portuguese.html";
+    }
+//END - Click Button Redict Page Portuguese
+//Click Flag Redict Page English
+const OPENUS = document.querySelector("select");
+OPENUS.addEventListener("change", englishPage);
+    function englishPage() {
+        location.href = "../english/english.html";
+    }
+//END - Click Button Redict Page English 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Loop Welcome
 const typing = document.querySelector("#textwelcome");
 
 const messages = ["Welcome to Marchetti Codes", "Bem-vindo ao Marchetti Codes"];
@@ -23,29 +24,34 @@ const type = () => {
 }
 
 setInterval(type, 200)
-
-//
-
+//END - Loop Welcome
+//Change Language PT-BR
 function changeTextptbr() {
     document.querySelector("#textpEnUS").innerHTML = "Escolha seu idioma.";
     document.querySelector("#buttonportuguesetext").innerHTML = "Português (Brasil)";
     document.querySelector("#buttonenglishtext").innerHTML = "Inglês (EUA)";
     document.querySelector("#footertext").innerHTML = "Desenvolvido por Matheus L. Marchetti";
 }
+//END - Change Language PT-BR
+//Change Language EN-US
 function changeTextenus() {
     document.querySelector("#textpEnUS").innerHTML = "Choose your language.";
     document.querySelector("#buttonportuguesetext").innerHTML = "Portuguese (Brazil)";
     document.querySelector("#buttonenglishtext").innerHTML = "English (USA)";
     document.querySelector("#footertext").innerHTML = "Developed by Matheus L. Marchetti";
 }
-
+//END - Change Language EN-US
+//Click Button Redict Page Portuguese
 const BTNPTBR = document.querySelector("#buttonportuguese");
 BTNPTBR.addEventListener("click", portuguesePage);
     function portuguesePage() {
         location.href = "portuguese/portuguese.html";
     }
+//END - Click Button Redict Page Portuguese
+//Click Button Redict Page English
 const BTNENUS = document.querySelector("#buttonenglish");
 BTNENUS.addEventListener("click", englishPage);
     function englishPage() {
         location.href = "english/english.html";
     }
+//END - Click Button Redict Page English 
