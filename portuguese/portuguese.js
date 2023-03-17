6ae994c7092493f20513e49aae17448b4b85a4f4
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-17 16:43:59 -0400

    Update portuguese.js

--- portuguese/portuguese.js
+++ portuguese/portuguese.js
@@ -1,13 +1,13 @@
 //Click Flag Redict Page Portuguese
-const OPPTBR = document.querySelector("select");
-OPPTBR.addEventListener("change", portuguesePage);
+const OPPTBR = document.querySelector(".br");
+OPPTBR.addEventListener("click", portuguesePage);
     function portuguesePage() {
         location.href = "../portuguese/portuguese.html";
     }
 //END - Click Button Redict Page Portuguese
 //Click Flag Redict Page English
-const OPENUS = document.querySelector("select");
-OPENUS.addEventListener("change", englishPage);
+const OPENUS = document.querySelector(".us");
+OPENUS.addEventListener("click", englishPage);
     function englishPage() {
         location.href = "../english/english.html";
     }
