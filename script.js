11f81c4fddd6735d592228b35998de40752084c6
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-17 01:35:16 -0400

    Updates

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+const typing = document.querySelector("#textwelcome");
+
+const messages = ["Welcome to Marchetti Codes", "Bem-vindo ao Marchetti Codes"];
+
+let messageIndex = 0;
+let characterIndex = 0;
+let currentMessage = "";
+let currentCharacters = "";
+
+const type = () => {
+    if (messageIndex === messages.length) {
+        messageIndex = 0
+    }
+
+    currentMessage = messages[messageIndex]
+    currentCharacters = currentMessage.slice(0, characterIndex++)
+    typing.textContent = currentCharacters
+
+    if (currentCharacters.length === currentMessage.length) {
+        messageIndex++
+        characterIndex = 0
+    }
+}
+
+setInterval(type, 200)
+
+//
+
 function changeTextptbr() {
-    document.querySelector("#textH1EnUS").innerHTML = "Escolha seu idioma.";
+    document.querySelector("#textpEnUS").innerHTML = "Escolha seu idioma.";
     document.querySelector("#buttonportuguesetext").innerHTML = "Português (Brasil)";
     document.querySelector("#buttonenglishtext").innerHTML = "Inglês (EUA)";
     document.querySelector("#footertext").innerHTML = "Desenvolvido por Matheus L. Marchetti";
 }
 function changeTextenus() {
-    document.querySelector("#textH1EnUS").innerHTML = "Choose your language.";
+    document.querySelector("#textpEnUS").innerHTML = "Choose your language.";
     document.querySelector("#buttonportuguesetext").innerHTML = "Portuguese (Brazil)";
     document.querySelector("#buttonenglishtext").innerHTML = "English (USA)";
     document.querySelector("#footertext").innerHTML = "Developed by Matheus L. Marchetti";
