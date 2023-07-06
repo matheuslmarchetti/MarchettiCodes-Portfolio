7c6470f2265dd3f4d8df5c5648938ae87bd82be1
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-07-06 14:52:47 -0400

    Change effect text CSS for JS

--- english/english.js
+++ english/english.js
@@ -5,3 +5,29 @@ OPPTBR.addEventListener("click", portuguesePage);
         location.href = "../portuguese/portuguese.html";
     }
 //END - Click Button Redict Page Portuguese
+//Loop Developer
+const typing = document.querySelector("#textdeveloper");
+
+const messages = ["Matheus L. Marchetti, developer."];
+
+let messageIndex = 0;
+let characterIndex = 0;
+let currentMessage = "";
+let currentCharacters = "";
+
+const type = () => {
+    if (messageIndex === messages.length) {
+        messageIndex = 0
+    }
+
+    currentMessage = messages[messageIndex]
+    currentCharacters = currentMessage.slice(0, characterIndex++)
+    typing.textContent = currentCharacters
+
+    if (currentCharacters.length === currentMessage.length) {
+        messageIndex;
+    }
+}
+
+setInterval(type, 200)
+//END - Loop Developer
--- portuguese/portuguese.js
+++ portuguese/portuguese.js
@@ -4,4 +4,30 @@ OPENUS.addEventListener("click", englishPage);
     function englishPage() {
         location.href = "../english/english.html";
     }
-//END - Click Button Redict Page English 
+//END - Click Button Redict Page English
+//Loop Developer
+const typing = document.querySelector("#textdeveloper");
+
+const messages = ["Matheus L. Marchetti, desenvolvedor."];
+
+let messageIndex = 0;
+let characterIndex = 0;
+let currentMessage = "";
+let currentCharacters = "";
+
+const type = () => {
+    if (messageIndex === messages.length) {
+        messageIndex = 0
+    }
+
+    currentMessage = messages[messageIndex]
+    currentCharacters = currentMessage.slice(0, characterIndex++)
+    typing.textContent = currentCharacters
+
+    if (currentCharacters.length === currentMessage.length) {
+        messageIndex;
+    }
+}
+
+setInterval(type, 200)
+//END - Loop Developer
